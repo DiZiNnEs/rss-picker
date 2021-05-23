8f9e24df66771666b6c8100d259dc1e76aa7f0be
dizinnes <dizinnes228@gmail.com>
2021-05-23 21:09:37 +0600

    feat(app): Create model of news

--- rss_app/models.py
+++ rss_app/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=64)
+    data_published = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'news'
+        verbose_name = 'news'
+        verbose_name_plural = 'news'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Comment from {self.author}'
+
+    class Meta:
+        db_table = 'comment'
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
