f5d38371c2fc619e7dd682ddc7db5d7e700fcafe
dizinnes <dizinnes228@gmail.com>
2021-05-25 23:12:10 +0600

    refactor(app/url): Change url path

--- rss_app/urls.py
+++ rss_app/urls.py
@@ -4,6 +4,6 @@ from rss_app.rss import LatestNewsFeed
 from rss_app.views import NewsDetail
 
 urlpatterns = [
-    path('detail-news/<slug>/', NewsDetail.as_view(), name='detail-news'),
+    path('detail/<slug>/', NewsDetail.as_view(), name='detail-news'),
     path('feed/', LatestNewsFeed()),
 ]
