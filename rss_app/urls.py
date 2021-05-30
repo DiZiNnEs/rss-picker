7a840c81d87983e14223315509da61b3f35cde61
dizinnes <dizinnes228@gmail.com>
2021-05-30 13:44:11 +0600

    feat(app/models): Create NewsView

--- rss_app/urls.py
+++ rss_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from rss_app.rss import LatestNewsFeed
-from rss_app.views import NewsDetail
+from rss_app.views import NewsView, NewsDetail
 
 urlpatterns = [
+    path('', NewsView.as_view(), name='index'),
     path('detail/<slug>/', NewsDetail.as_view(), name='detail-news'),
     path('feed/', LatestNewsFeed()),
 ]
--- rss_app/views.py
+++ rss_app/views.py
@@ -1,10 +1,14 @@
-from django.views.generic import DetailView
+from django.views import generic
 
 from rss_app.models import News
 
 
-class NewsDetail(DetailView):
+class NewsView(generic.ListView):
+    template_name = 'index.html'
+    model = News
+    context_object_name = 'news_list'
+
+
+class NewsDetail(generic.DetailView):
     template_name = 'news_detail.html'
     model = News
-
-
