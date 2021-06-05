726162ae3797c15fcd420249224d285aa2330498
dizinnes <dizinnes228@gmail.com>
2021-06-05 21:02:52 +0600

    feat(rss_picker/test): Create test for views

---
+++ rss_app/tests/views_test.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from rss_app.views import NewsView
+
+class AuthorListViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
