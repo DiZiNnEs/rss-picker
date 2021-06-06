fa8028afee6b9d47a4a64ec18966120e71170740
dizinnes <dizinnes228@gmail.com>
2021-06-06 20:24:34 +0600

    feat(rss_picker/test): Create test for `NewsDetail`

--- rss_app/tests/views_test.py
+++ rss_app/tests/views_test.py
@@ -12,3 +12,13 @@ class NewsViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
+
+class NewsDetailViewTestCase(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('detail/chto-takoe-python/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('detail-news'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'news_detail.html')
